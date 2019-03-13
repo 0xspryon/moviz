aff6b11d66bf4aec08bd99dd2a45be70fb18ecb6
Yonga Springfield <springfieldyonga@outlook.com>
2019-03-13 19:52:46 +0100

    finished everything on moviz

---
+++ js/config.js
@@ -0,0 +1,11 @@
+/*
+*** This file sets the configuration variables for application.
+*** Replace the API KEY variable with your own key if you wish
+*** to use this code. In a real production environment this file
+*** would not be publically available on Github.
+*** List this file in a .gitignore
+*/
+
+const API_KEY = '77d0ffc60508a23f1198f90b09f3f330';
+const BASE_URL = 'https://api.themoviedb.org/3/';
+const BASE_IMG_URL = 'https://image.tmdb.org/t/p/';
